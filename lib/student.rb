254e278776511d3da0d040f20c11b4c6c23c312d
Nolan Hughes <nhughes987@gmail.com>
2018-01-09 21:48:33 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,9 +39,9 @@ class Student
       WHERE grade = 9
       SQL
 
-      DB[:conn].execute(sql, name).map do |row|
+      DB[:conn].execute(sql).map do |row|
         self.new_from_db(row)
-      end.first
+      end
   end
 
   def save
